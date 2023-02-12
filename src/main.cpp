37b73f21dddba39bdcd9864f7cfcf619c530af86
Peter Kang <peterkyungmo@gmail.com>
2023-02-12 16:16:03 -0500

    added texture rendering and primitive drawing examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <cmath>
 
-enum KeyPressSurfaces {
-    KEY_PRESS_SURFACE_DEFAULT,
-    KEY_PRESS_SURFACE_UP,
-    KEY_PRESS_SURFACE_DOWN,
-    KEY_PRESS_SURFACE_LEFT,
-    KEY_PRESS_SURFACE_RIGHT,
-    KEY_PRESS_SURFACE_TOTAL
-};
-
-bool init();
-bool loadMedia();
-SDL_Surface* loadSurface(std::string path);
-void close();
-
+//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+// function declarations
+bool init();
+bool loadMedia();
+void close();
+SDL_Texture* loadTexture (std::string path);
+
+// parameter declarations
 SDL_Window* gWindow = nullptr;
-SDL_Surface* gScreenSurface = nullptr;
-SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
-SDL_Surface* gCurrentSurface = nullptr;
+SDL_Renderer* gRenderer = nullptr;
 
 bool init() {
     //Initialization flag
@@ -34,8 +30,17 @@ bool init() {
     }
     else
     {
+        //Set texture filtering to linear
+        if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+            printf("Warning: linear texture filtering not enabled!");
+        }
         //Create window
-        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        gWindow = SDL_CreateWindow( "SDL Tutorial",
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SDL_WINDOWPOS_UNDEFINED,
+                                    SCREEN_WIDTH,
+                                    SCREEN_HEIGHT,
+                                    SDL_WINDOW_SHOWN );
         if( gWindow == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
@@ -43,20 +48,26 @@ bool init() {
         }
         else
         {
-            //Get window surface
-            gScreenSurface = SDL_GetWindowSurface( gWindow );
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+            if (gRenderer == NULL) {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = false;
+            } else {
+                // Initialize renderer color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                    success = false;
+                }
+            }
         }
     }
     return success;
-}
-
-SDL_Surface* loadSurface (std::string path) {
-    //Load image at specified path
-    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
-    if(loadedSurface == nullptr) {
-        printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-    }
-    return loadedSurface;
 }
 
 bool loadMedia()
@@ -64,59 +75,39 @@ bool loadMedia()
     //Loading success flag
     bool success = true;
 
-    // Load up surface
-    gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("images/press.bmp");
-    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == nullptr) {
-        printf("Failed to load up image!\n");
-        success = false;
-    }
-
-    //Load up surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] = loadSurface( "images/up.bmp" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ] == NULL )
-    {
-        printf( "Failed to load up image!\n" );
-        success = false;
-    }
-
-    //Load down surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] = loadSurface( "images/down.bmp" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ] == NULL )
-    {
-        printf( "Failed to load down image!\n" );
-        success = false;
-    }
-
-    //Load left surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] = loadSurface( "images/left.bmp" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ] == NULL )
-    {
-        printf( "Failed to load left image!\n" );
-        success = false;
-    }
-
-    //Load right surface
-    gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] = loadSurface( "images/right.bmp" );
-    if( gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ] == NULL )
-    {
-        printf( "Failed to load right image!\n" );
-        success = false;
-    }
-
+    //Nothing to load
     return success;
+}
+
+SDL_Texture* loadTexture(std::string path) {
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if (loadedSurface == NULL) {
+        printf("Unable to load image %s! SDL_image Error %s\n", path.c_str(), IMG_GetError());
+    } else {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        if (newTexture == NULL) {
+            printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+        }
+        //Get rid of old loaded surface
+        SDL_FreeSurface(loadedSurface);
+    }
+    return newTexture;
 }
 
 void close()
 {
-    //Deallocate surface
-    SDL_FreeSurface( gCurrentSurface );
-    gCurrentSurface = NULL;
-
     //Destroy window
+    SDL_DestroyRenderer(gRenderer);
     SDL_DestroyWindow( gWindow );
     gWindow = NULL;
+    gRenderer = NULL;
 
     //Quit SDL subsystems
+    IMG_Quit();
     SDL_Quit();
 }
 
@@ -139,43 +130,44 @@ int main(int argc, char* args[]) {
             bool quit = false;
             //Event handler
             SDL_Event e;
-            //Set default current surface
-            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
             //While application is running
             while(!quit) {
                 //Handle events on queue
                 while(SDL_PollEvent(&e) != 0) {
-                    if(e.type == SDL_QUIT) {
+                    if (e.type == SDL_QUIT) {
                         quit = true;
-                    } else if(e.type == SDL_KEYDOWN) {
-                        //Select surfaces based on key press
-                        switch(e.key.keysym.sym) {
-                            case SDLK_UP:
-                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_UP ];
-                                break;
-
-                            case SDLK_DOWN:
-                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DOWN ];
-                                break;
-
-                            case SDLK_LEFT:
-                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_LEFT ];
-                                break;
-
-                            case SDLK_RIGHT:
-                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_RIGHT ];
-                                break;
-
-                            default:
-                                gCurrentSurface = gKeyPressSurfaces[ KEY_PRESS_SURFACE_DEFAULT ];
-                                break;
-                        }
                     }
                 }
-                //Apply the image
-                SDL_BlitSurface( gCurrentSurface, NULL, gScreenSurface, NULL );
-                //Update the surface
-                SDL_UpdateWindowSurface( gWindow );
+                //Clear screen
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                SDL_RenderClear(gRenderer);
+
+                //Render red filled quad
+                SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4,
+                                      SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+                SDL_RenderFillRect(gRenderer, &fillRect);
+
+                //Render green outlined quad
+                SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6,
+                                        SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
+                SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+                SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+                //Draw blue horizontal line
+                SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+                SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2,
+                                   SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+                // Draw vertical line of yellow dots
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+                for (int i = 0; i < SCREEN_HEIGHT; i += 4) {
+                    SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+                }
+
+                //Update screen
+                SDL_RenderPresent(gRenderer);
             }
         }
     }
