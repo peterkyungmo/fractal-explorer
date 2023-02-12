34f733e7bb98ce5cb64c29c4ee503e90b6cd3000
Peter Kang <peterkyungmo@gmail.com>
2023-02-12 13:46:17 -0500

    fixed hello_world image and pathing

--- src/main.cpp
+++ src/main.cpp
@@ -44,10 +44,10 @@ bool loadMedia()
     //Loading success flag
     bool success = true;
     //Load splash image
-    gHelloWorld = SDL_LoadBMP( "02_getting_an_image_on_the_screen/hello_world.bmp" );
+    gHelloWorld = SDL_LoadBMP( "images/hello_world.bmp" );
     if( gHelloWorld == NULL )
     {
-        printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+        printf( "Unable to load image %s! SDL Error: %s\n", "images/hello_world.bmp", SDL_GetError() );
         success = false;
     }
     return success;
